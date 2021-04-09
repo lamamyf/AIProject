34288f6c7a1cb533994ee9248d387d2e9a2ec039
Noha AlDossari <60599498+Noha66@users.noreply.github.com>
2021-04-09 19:42:58 +0300

    trying..

--- src/sourceCode/MainClass.java
+++ src/sourceCode/MainClass.java
@@ -6,6 +6,7 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+    System.out.println("_______________________________");
     System.out.println("Start program");
     System.out.println("_______________________________");
     readBinaryTreeFromFile();
